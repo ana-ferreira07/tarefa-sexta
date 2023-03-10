a37d00f9a761bfe3b0a943556018fb1a79eb0c32
ANA BEATRIZ FERREIRA PASSOS <passos.2967@aluno.pr.senac.br>
2023-03-10 08:12:47 -0300

    dieimes

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+    $a = 1;
+    $b = 2;
+    $c = 3;
+    $d = 4;
+    $e = 5;
+    $f = 6;
+    $g = 7;
+    $h = 8;
+    $i = 9;
+    $j = 10;
+
+    echo "tabuada do 1";
+    echo("</br>");
+    while ($a <=10){
+        echo " Numero = " . $a . "</br>";
+        $a = $a +1;
+    }
+    echo("</br>");
+    echo "tabuada do 2";
+    echo("</br>");
+    while ($b <=20){
+        echo " Numero = " . $b . "</br>";
+        $b = $b +2;
+    }
+    echo("</br>");
+    echo "tabuada do 3";
+    echo("</br>");
+    while ($c <=30){
+        echo " Numero = " . $c . "</br>";
+        $c = $c +3;
+    }
+    echo("</br>");
+    echo "tabuada do 4";
+    echo("</br>");
+    
+    while ($d <=40){
+        echo " Numero = " . $d . "</br>";
+        $d = $d +4;
+    }
+    echo("</br>");
+    echo "tabuada do 5";
+    echo("</br>");
+    
+    while ($e <=50){
+        echo " Numero = " . $e . "</br>";
+        $e = $e +5;
+    }
+    echo("</br>");
+    echo "tabuada do 6";
+    echo("</br>");
+    
+    while ($f <=60){
+        echo " Numero = " . $f . "</br>";
+        $f = $f +6;
+    }
+    echo("</br>");
+    echo "tabuada do 7";
+    echo("</br>");
+    
+    while ($g <=70){
+        echo " Numero = " . $g . "</br>";
+        $g = $g +7;
+    }
+    echo("</br>");
+    echo "tabuada do 8";
+    echo("</br>");
+    
+    while ($h <=80){
+        echo " Numero = " . $h . "</br>";
+        $h = $h +8;
+    }
+    echo("</br>");
+    echo "tabuada do 9";
+    echo("</br>");
+    
+    while ($i <=80){
+        echo " Numero = " . $i . "</br>";
+        $i = $i +9;
+    }
+    echo("</br>");
+    echo "tabuada do 10";
+    echo("</br>");
+    
+    while ($j <=100){
+        echo " Numero = " . $j . "</br>";
+        $j = $j +10;
+    }
+    ?>
+    
+    
+
+    
